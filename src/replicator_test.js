f76a05d44d2b658b5a2fe3ed9cd82ce15a5726c9
Peter Boyer <peter.b.boyer@gmail.com>
2015-08-08 18:35:21 -0400

    More replicator_test

--- src/replicator_test.js
+++ src/replicator_test.js
@@ -7,19 +7,30 @@ function run(f, a){
 	return r.replicate(f,a);
 }
 
-var doubleIt = new types.TypedFunction(function(a){ return 2 * a; }, [new types.TypedArgument("a", "number")], "doubleIt" );
-var addArr = new types.TypedFunction(function(a){ return a[0] + a[1]; }, [new types.TypedArgument("a", "number[]")], "addArr" );
-
+var doubleIt = 
+	new types.TypedFunction(
+		function(a){ return 2 * a; }, 
+		[new types.TypedArgument("a", "number")] );
 assert.deepEqual( 4, run(doubleIt, [ 2 ]));	
 assert.throws( function(){ run(doubleIt, [ true ]) });	
 assert.deepEqual( [4,8], run(doubleIt, [[2, 4]]));	
+
+var addArr = 
+	new types.TypedFunction(
+		function(a){ return a[0] + a[1]; }, 
+		[new types.TypedArgument("a", "number[]")] );
 assert.deepEqual( 6, run(addArr, [[2, 4]]));	
 
 var addBoth = 
 	new types.TypedFunction(
 		function(a,b){ return [a[0]+b, a[1]+b]; }, 
-		[new types.TypedArgument("a", "number[]"), new types.TypedArgument("b", "number")], 
-		"addBoth" );
+		[ 	new types.TypedArgument("a", "number[]"), 
+			new types.TypedArgument("b", "number")]);
 assert.deepEqual( [4,6], run(addBoth, [[2, 4], 2]));	
 
-
+var mixedTypes = 
+	new types.TypedFunction(
+		function(a,b){ return b ? a : -1; }, 
+		[	new types.TypedArgument("a", "number"), 
+			new types.TypedArgument("b", "boolean")]);
+assert.deepEqual( 12, run(mixedTypes, [12, true]));	
