e3434d9b0487ef01694edd72f029ea826beeb79b
Peter Boyer <peter.b.boyer@gmail.com>
2015-08-22 10:50:44 -0400

    More cps

--- src/CpsVisitor.js
+++ src/CpsVisitor.js
@@ -1,46 +1,51 @@
 var Environment_1 = require('./Environment');
+var RuntimeTypes_1 = require('./RuntimeTypes');
 var CpsInterpreter = (function () {
-    function CpsInterpreter() {
+    function CpsInterpreter(debug) {
         this.env = new Environment_1.Environment();
         // default continuation
-        this.cc = function (a, c) {
-            // console.log("OK");
-            // console.log(a);
-            c();
+        this.debug = function (a, ret) {
+            // by default
+            ret();
         };
+        if (debug) {
+            this.debug = debug;
+        }
     }
-    // passes control to any external party before continuing
+    // passes control to someone else
     CpsInterpreter.prototype.step = function (node, ret) {
-        this.cc(node, ret);
+        if (this.debug) {
+            this.debug(node, ret);
+        }
+        else {
+            ret();
+        }
     };
     CpsInterpreter.prototype.visitStatementListNode = function (node, ret) {
         var _this = this;
-        console.log("StatementListNode");
         this.step(node, function () {
-            var iterate = function (n) {
+            var iterate = function (n, r) {
                 if (!n.head) {
-                    return ret(null);
+                    ret(r);
                 }
-                n.head.cpsAccept(_this, function (_) { return iterate(n.tail); });
+                else {
+                    n.head.cpsAccept(_this, function (x) { return iterate(n.tail, x); });
+                }
             };
-            iterate(node);
+            iterate(node, undefined);
         });
     };
     CpsInterpreter.prototype.visitIdentifierNode = function (node, ret) {
         var _this = this;
-        console.log("IdentifierNode");
         this.step(node, function () {
-            console.log("looking up", node.name);
             ret(_this.env.lookup(node.name));
         });
     };
     CpsInterpreter.prototype.visitNumberNode = function (node, ret) {
-        console.log("NumberNode");
         this.step(node, function () { return ret(node.value); });
     };
     CpsInterpreter.prototype.visitBinaryExpressionNode = function (node, ret) {
         var _this = this;
-        console.log("BinaryExpressionNode");
         this.step(node, function () {
             // evaluate first expression
             node.firstExpression.cpsAccept(_this, 
@@ -48,7 +53,9 @@ var CpsInterpreter = (function () {
             // evaluate second
             function (a) { return node.secondExpression.cpsAccept(_this, 
             // return the sum
+            // TODO include all of the type operators
             // return the sum
+            // TODO include all of the type operators
             function (b) { return ret(a + b); }); });
         });
     };
@@ -57,26 +64,100 @@ var CpsInterpreter = (function () {
         this.step(node, function () {
             // evaluate first expression
             node.expression.cpsAccept(_this, 
-            // store
-            // store
-            function (e) { return ret(_this.env.set(node.identifier.name, e)); });
+            // store the value
+            // store the value
+            function (e) {
+                _this.env.set(node.identifier.name, e);
+                ret(e);
+            });
         });
     };
-    CpsInterpreter.prototype.visitIdentifierListNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitBooleanNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitStringNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitArrayNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitRangeExpressionNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitFunctionCallNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitArrayIndexNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitExpressionListNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitIfStatementNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitFunctionDefinitionNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitReplicationExpressionNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitReplicationGuideNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitReplicationGuideListNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitImperativeBlockNode = function (node, c) { throw new Error("Not implemented"); };
-    CpsInterpreter.prototype.visitAssociativeBlockNode = function (node, c) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.visitFunctionCallNode = function (node, ret) {
+        var _this = this;
+        this.step(node, function () {
+            var f = _this.env.lookup(node.functionId.name);
+            node.arguments.cpsAccept(_this, function (args) {
+                args.push(ret); // last arg is the continuation
+                f.func.apply(undefined, args);
+            });
+        });
+        // Replicator.cpsReplicate(f, args, ret)
+    };
+    CpsInterpreter.prototype.visitExpressionListNode = function (node, ret) {
+        var _this = this;
+        this.step(node, function () {
+            var iterate = function (n, a) {
+                if (!n.head) {
+                    ret(a);
+                }
+                else {
+                    n.head.cpsAccept(_this, function (x) {
+                        a.push(x);
+                        iterate(n.tail, a);
+                    });
+                }
+            };
+            iterate(node, []);
+        });
+    };
+    CpsInterpreter.prototype.visitFunctionDefinitionNode = function (fds, ret) {
+        // unpack the argument list 
+        var il = fds.arguments;
+        var val = [];
+        while (il != undefined) {
+            var t = il.head.type;
+            val.push(new RuntimeTypes_1.TypedArgument(il.head.name, t ? t.name : undefined));
+            il = il.tail;
+        }
+        var fd;
+        var env = this.env;
+        var interpreter = this;
+        function f() {
+            // sad, but necessary
+            var args = Array.prototype.slice.call(arguments);
+            var r = args.pop(); // pull out the continuation
+            interpreter.apply(fds, env, args, r);
+        }
+        // recursion
+        env.set(fds.identifier.name, f);
+        fd = new RuntimeTypes_1.TypedFunction(f, val, fds.identifier.name);
+        this.env.set(fds.identifier.name, fd);
+        ret(undefined);
+    };
+    CpsInterpreter.prototype.apply = function (fd, env, args, ret) {
+        var _this = this;
+        env = new Environment_1.Environment(env);
+        // bind the arguments in the scope 
+        var i = 0;
+        var il = fd.arguments;
+        while (il != null) {
+            env.set(il.head.name, args[i++]);
+            il = il.tail;
+        }
+        ;
+        var current = this.env;
+        this.env = env;
+        fd.body.cpsAccept(this, function (x) {
+            _this.env = current;
+            ret(x);
+        });
+    };
+    CpsInterpreter.prototype.visitIdentifierListNode = function (node, ret) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.visitArrayIndexNode = function (node, ret) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.visitIfStatementNode = function (node, ret) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.visitReplicationExpressionNode = function (node, ret) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.visitReplicationGuideNode = function (node, ret) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.visitReplicationGuideListNode = function (node, ret) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.visitImperativeBlockNode = function (node, ret) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.visitAssociativeBlockNode = function (node, ret) { throw new Error("Not implemented"); };
+    // easy
+    CpsInterpreter.prototype.visitBooleanNode = function (node, ret) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.visitStringNode = function (node, ret) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.visitArrayNode = function (node, ret) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.visitRangeExpressionNode = function (node, ret) { throw new Error("Not implemented"); };
+    CpsInterpreter.prototype.error = function (message, state) {
+        return new RuntimeTypes_1.DesignScriptError(message, state);
+    };
     return CpsInterpreter;
 })();
 exports.CpsInterpreter = CpsInterpreter;
--- src/CpsVisitor.ts
+++ src/CpsVisitor.ts
@@ -56,7 +56,7 @@ export class CpsInterpreter implements CpsVisitor<any> {
             () => {  
                 var iterate = (n,r) => {
                     if (!n.head) {
-                        return ret( r );
+                        ret( r );
                     } else {
                         n.head.cpsAccept( this, (x) => iterate( n.tail, x ) );
                     }
@@ -115,40 +115,90 @@ export class CpsInterpreter implements CpsVisitor<any> {
     visitFunctionCallNode(node : AST.FunctionCallNode, ret : (T) => void){ 
         this.step( node, 
 			() => {
-                var f = this.env.lookup( node.functionId.name );
-                node.arguments.cpsAccept(this, (args) => ret( Replicator.replicate(f, args) ));
+                var f : TypedFunction = this.env.lookup( node.functionId.name );
+                
+                node.arguments.cpsAccept(this, (args) => {
+                    args.push(ret); // last arg is the continuation
+                    f.func.apply( undefined, args );
+                });
 			}
 		);
+        // Replicator.cpsReplicate(f, args, ret)
     }
 
     visitExpressionListNode(node : AST.ExpressionListNode, ret : (T) => void){ 
         this.step( node, 
 			() => {
-                var args = [];
-                var iterate = (n) => {
+                var iterate = (n, a) => {
                     if (!n.head) {
-                        return ret(args);
+                        ret(a);
                     } else {
                         n.head.cpsAccept( this, (x) => { 
-                            args.push(x); 
-                            iterate( n.tail );  
+                            a.push(x); 
+                            iterate( n.tail, a );  
                         });
                     }
                 };
                 
-                iterate( node );
+                iterate( node, [] );
 			}
 		);
     }
     
-    visitFunctionDefinitionNode(node : AST.FunctionDefinitionNode, ret : (T) => void){ 
+    visitFunctionDefinitionNode(fds : AST.FunctionDefinitionNode, ret : (T) => void){ 
+ 
+        // unpack the argument list 
+        var il = fds.arguments;
+        var val = [];
+        while (il != undefined) {
+            var t = il.head.type;
+            val.push(new TypedArgument(il.head.name, t ? t.name : undefined));
+
+            il = il.tail;
+        }
+
+        var fd;
+        var env = this.env;
+        var interpreter = this;
+
+        function f() {
+            // sad, but necessary
+            var args = Array.prototype.slice.call(arguments);
+            
+            var r = args.pop(); // pull out the continuation
+            interpreter.apply(fds, env, args, r);
+        }
         
+        // recursion
+        env.set(fds.identifier.name, f);
+
+        fd = new TypedFunction(f, val, fds.identifier.name);
+
+        this.env.set(fds.identifier.name, fd);
+        
+        ret(undefined);
     }
     
-    apply(f : TypedFunction, args : any[], ret : (T) => void ){
-        
+    apply(fd: AST.FunctionDefinitionNode, env: Environment, args: any[], ret : (T) => void ){
+        env = new Environment(env);
+
+        // bind the arguments in the scope 
+        var i = 0;
+        var il = fd.arguments;
+        while (il != null) {
+            env.set(il.head.name, args[i++]);
+            il = il.tail;
+        };
+
+        var current = this.env;
+        this.env = env;
+
+        fd.body.cpsAccept(this, (x) => {
+            this.env = current;
+            ret(x)
+        });
     }
-    	
+    
     visitIdentifierListNode(node : AST.IdentifierListNode, ret : (T) => void){ throw new Error("Not implemented"); }
     visitArrayIndexNode(node : AST.ArrayIndexNode, ret : (T) => void){ throw new Error("Not implemented"); }
     visitIfStatementNode(node : AST.IfStatementNode, ret : (T) => void){ throw new Error("Not implemented"); }
